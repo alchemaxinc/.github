20ce11b2a7c1d270b1b79fb2392ed4bc0d29be4b
Kaspar Lyngsie <github@pacroon.dk>
2025-12-01 21:37:01 +0100

    fix: adding templates, static images, favicons

--- web/pandoc/filters/year.lua
+++ web/pandoc/filters/year.lua
@@ -1,5 +1,11 @@
 local current_year = os.date('%Y')
 
+-- Set the year in metadata so templates can access it
+function Meta(meta)
+  meta.year = pandoc.MetaString(current_year)
+  return meta
+end
+
 function Str(el)
   if el.text and el.text:match('CURRENT_YEAR') then
     el.text = el.text:gsub('CURRENT_YEAR', current_year)
@@ -13,3 +19,11 @@ function RawInline(el)
   end
   return el
 end
+
+function RawBlock(el)
+  if el.text and el.text:match('CURRENT_YEAR') then
+    el.text = el.text:gsub('CURRENT_YEAR', current_year)
+  end
+  return el
+end
+
